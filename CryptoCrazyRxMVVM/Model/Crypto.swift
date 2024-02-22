2ad977df00f2bfc770cc2d9aa25c4a6df029f449
Mahmut Ersoy <mahmutersooy@gmail.com>
2024-02-22 16:35:40 +0300

    Added MVVM architecture, web service integration, and UI design

---
+++ CryptoCrazyRxMVVM/Model/Crypto.swift
@@ -0,0 +1,13 @@
+//
+//  Crypto.swift
+//  CryptoCrazyRxMVVM
+//
+//  Created by Mahmut Ersoy on 22.02.2024.
+//
+
+import Foundation
+
+struct Crypto : Codable {
+    let currency : String
+    let price : String
+}
---
+++ CryptoCrazyRxMVVM/View/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  CryptoCrazyRxMVVM
+//
+//  Created by Mahmut Ersoy on 22.02.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        var content = cell.defaultContentConfiguration()
+        content.text = "Crypto Currency"
+        content.secondaryText = "Crypto Price"
+        cell.contentConfiguration = content
+        return cell
+    }
+
+}
+
