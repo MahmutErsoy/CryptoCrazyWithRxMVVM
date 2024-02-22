3e987818fd2b737119a163ab3090528215b79daa
Mahmut Ersoy <mahmutersooy@gmail.com>
2024-02-22 18:36:13 +0300

    Implemented CryptoViewModel to handle cryptocurrency data retrieval and error handling. Utilized RxSwift and RxCocoa for reactive bindings in CryptoListViewController.

--- CryptoCrazyRxMVVM/View/ViewController.swift
+++ CryptoCrazyRxMVVM/View/ViewController.swift
@@ -6,25 +6,55 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     @IBOutlet weak var tableView: UITableView!
     
+    var cryptoList = [Crypto]()
+    
+    let cryptoVM = CryptoViewModel()
+    let disposeBag = DisposeBag()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        setupBindings()
+        cryptoVM.requestData()
+    }
+    
+    private func setupBindings() {
+        
+        cryptoVM
+            .error
+            .observe(on: MainScheduler.asyncInstance)
+            .subscribe { errorString in
+                print(errorString)
+            }
+            .disposed(by: disposeBag)
+        
+        cryptoVM
+            .cryptos
+            .observe(on: MainScheduler.asyncInstance)
+            .subscribe { cryptos in
+                self.cryptoList = cryptos
+                self.tableView.reloadData()
+            }
+            .disposed(by: disposeBag)
     }
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return cryptoList.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell()
         var content = cell.defaultContentConfiguration()
-        content.text = "Crypto Currency"
-        content.secondaryText = "Crypto Price"
+        content.text = cryptoList[indexPath.row].currency
+        content.secondaryText = cryptoList[indexPath.row].price
         cell.contentConfiguration = content
         return cell
     }
